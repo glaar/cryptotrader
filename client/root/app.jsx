890ef41b05bb3870f33a18d296dd0c833a6c2c8b
Glenn Aarøen <glaar90@gmail.com>
2017-10-14 13:01:29 +0200

    working refactor

---
+++ client/root/app.jsx
@@ -0,0 +1,28 @@
+import React, {Component} from 'react';
+import ReactDOM from 'react-dom';
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
+import MyAwesomeReactComponent from './layout/MyAwesomeReactComponent';
+import lightBaseTheme from 'material-ui/styles/baseThemes/lightBaseTheme';
+import getMuiTheme from 'material-ui/styles/getMuiTheme';
+import AppBar from 'material-ui/AppBar';
+
+class App extends Component {
+    render() {
+        return (
+            <div>
+                <MuiThemeProvider muiTheme={getMuiTheme(lightBaseTheme)}>
+                    <AppBar title="My AppBar"/>
+                </MuiThemeProvider>
+                <MuiThemeProvider muiTheme={getMuiTheme(lightBaseTheme)}>
+                    <MyAwesomeReactComponent/>
+                </MuiThemeProvider>
+
+            </div>
+        )
+    }
+}
+
+ReactDOM.render(
+    <App />,
+    document.getElementById('app')
+);
--- webpack.config.js
+++ webpack.config.js
@@ -5,7 +5,7 @@ var BundleTracker = require('webpack-bundle-tracker');
 module.exports = {
   context: __dirname,
 
-  entry: './assets/js/index', // entry point of our app. assets/js/index.js should require other js modules and dependencies it needs
+  entry: './client/root/app', // entry point of our app. assets/js/index.js should require other js modules and dependencies it needs
 
   output: {
       path: path.resolve('./assets/bundles/'),
