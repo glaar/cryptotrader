5a50bb45aa53f72dd52d276bf123f48a23c3cff0
Glenn Aarøen <glaar90@gmail.com>
2017-10-18 12:16:26 +0200

    start

--- api/views.py
+++ api/views.py
@@ -1,4 +1,11 @@
 from django.shortcuts import render
+from bittrex import Bittrex
+
+
+my_bittrex = Bittrex(None, None, api_version=API_V2_0)  # or defaulting to v1.1 as Bittrex(None, None)
+my_bittrex.get_markets()
+
+print(my_bittrex)
 
 
 import requests
@@ -12,3 +19,9 @@ def index(request):
     print(jdata['result'][0]['MarketName'])
 
     return render(request, 'api/index.html', context)
+
+
+def test(request):
+
+    context = {'wallet': '100'}
+    return render(request, 'api/test.html', context)
