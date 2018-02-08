71a66f2ffd0fca2c2e33a79d9322a05fbd8dea5f
Glenn Aarøen <glaar90@gmail.com>
2018-02-08 17:18:00 +0100

    installed bootstrap

--- client/index.jsx
+++ client/index.jsx
@@ -12,6 +12,7 @@ import Wallet from './wallet/wallet';
 import Autotrade from './autotrade/autotrade';
 
 import { Route, Switch, BrowserRouter } from 'react-router-dom';
+import {ButtonToolbar, DropdownButton, MenuItem} from "react-bootstrap";
 
 require('../assets/css/main.css');
 
@@ -19,6 +20,21 @@ require('../assets/css/main.css');
 const Home = () => (
   <div>
     <h1>Cryptotrader</h1>
+
+      <ButtonToolbar>
+          <DropdownButton
+              bsStyle="default"
+              title="No caret"
+              noCaret
+              id="dropdown-no-caret">
+              <MenuItem eventKey="1">Action</MenuItem>
+              <MenuItem eventKey="2">Another action</MenuItem>
+              <MenuItem eventKey="3">Something else here</MenuItem>
+              <MenuItem divider />
+              <MenuItem eventKey="4">Separated link</MenuItem>
+          </DropdownButton>
+      </ButtonToolbar>
+
   </div>
 );
 
